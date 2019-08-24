c11b79ebd2e54ccd22e3f3c86347853c12273f1f
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-08-24 13:46:05 +0100

    Net-ninjs tut 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Center(
-        child: Text('Hello world'),
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Mosaic'),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Text('Hello world'),
+          ),
+          floatingActionButton: RaisedButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            onPressed: () => print('hi'),
+            focusColor: Colors.blue,
+            child: Text('Click'),
+          ),
+        ),
       ),
-    ));
+    );
