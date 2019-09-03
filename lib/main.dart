a97354fcb2cd31777e3d7b4a42c2b2251e9ca5ef
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-09-03 04:01:27 +0100

    Episode 14

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Mosaic'),
-            centerTitle: true,
-          ),
-          body: Center(
-            child: Text('Hello world'),
-          ),
-          floatingActionButton: RaisedButton(
-            color: Colors.blue,
-            textColor: Colors.white,
-            onPressed: () => print('hi'),
-            focusColor: Colors.blue,
-            child: Text('Click'),
-          ),
-        ),
+        theme: ThemeData(primaryColor: Colors.red[500]),
+        home: MyApp(),
       ),
     );
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mosaic'),
+        centerTitle: true,
+      ),
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text('1'),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.pinkAccent,
+              child: Text('2'),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.amber,
+              child: Text('3'),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[600],
+        onPressed: () {},
+        child: Text('Click'),
+      ),
+    );
+  }
+}
