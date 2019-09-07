6e5927d9d0c83316973006ba195cd69f768dca93
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-09-07 03:20:25 +0100

    Using Lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Center(
-        child: Text('Hello word!'),
+import 'quote.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: QuoteList(),
       ),
-    ));
+    );
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(
+        author: 'Oscar Wilde',
+        text: 'Be yourself; everyone else is already taken'),
+    Quote(
+        author: 'Oscar Wilde',
+        text: 'I have nothing to declare except my genuis'),
+    Quote(
+        author: 'Oscar Wilde',
+        text: 'The truth is rarely pure and never simple'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+      ),
+    );
+  }
+}
